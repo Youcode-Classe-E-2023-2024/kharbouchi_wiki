0158b5d66e80c46a55eb6d46d7e5ef71075e04c1
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-01-13 15:09:35 +0100

    back-end

---
+++ file1.js
@@ -0,0 +1,5 @@
+
+var URL_ROOT = URL_ROOT || {};
+
+// Define a global variable within the namespace
+URL_ROOT.globalVariable = "Hello from file1!";
---
+++ file2.js
@@ -0,0 +1,7 @@
+
+var URL_ROOT = URL_ROOT || {};
+
+
+console.log(URL_ROOT.globalVariable); 
+
+
